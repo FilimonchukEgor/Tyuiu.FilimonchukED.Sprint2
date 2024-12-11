773809ec37c32cc3a13f6d5d32102e299a7e96f6
Ернар Пупкин <gamerhey2@gmail.com>
2024-12-11 21:12:22 +0500

    Task0

--- Tyuiu.FilimonchukED.Sprint2.Task0.V11.Lib/DataService.cs
+++ Tyuiu.FilimonchukED.Sprint2.Task0.V11.Lib/DataService.cs
@@ -8,10 +8,10 @@ namespace Tyuiu.FilimonchukED.Sprint2.Task0.V11.Lib
             bool[] res = new bool[6];
             res[0] = x - 7830 == y;
             res[1] = x - 7830 != y;
-            res[2] = x - 8000 < y;
-            res[3] = x - 8000 > y;
-            res[4] = x - 8000 <= y;
-            res[5] = x - 8000 >= y;
+            res[2] = x > y;
+            res[3] = x - 7830 > y;
+            res[4] = x - 7830 <= y;
+            res[5] = x / 10000 >= y;
 
             return res;
         }
