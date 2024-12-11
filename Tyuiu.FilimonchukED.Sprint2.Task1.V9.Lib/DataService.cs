85b3d7be60481133f5c359a91cf2b2256b527e6b
Ернар Пупкин <gamerhey2@gmail.com>
2024-12-11 22:05:18 +0500

    Task1

---
+++ Tyuiu.FilimonchukED.Sprint2.Task1.V9.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.FilimonchukED.Sprint2.Task1.V9.Lib
+{
+    public class DataService : ISprint2Task1V9
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+            res[0] = (a == b) | (b == c);
+            res[1] = (a == c) & (b == d);
+            res[2] = (a < d) || (a == d);
+            res[3] = (b > d) && (a == c);
+            res[4] = !res[0];
+            res[5] = (a < c) ^ (b != d);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.FilimonchukED.Sprint2.Task1.V9/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.FilimonchukED.Sprint2.Task1.V9.Lib;
+namespace Tyuiu.FilimonchukED.Sprint2.Task1.V9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Филимончук Е. Д. | РППб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Логические операции                                               *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Выполнил: Филимончук Егор Дмитриевич | РППб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*  a = 15; b = 335; c = 174; d = 478                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int a = 15; int b = 335; int c = 174; int d = 478;
+
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
