af33275ada62daf5874373ee70c7bb9470fdb760
Ернар Пупкин <gamerhey2@gmail.com>
2024-12-12 17:32:03 +0500

    Task5

---
+++ Tyuiu.FilimonchukED.Sprint2.Task5.V15.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.FilimonchukED.Sprint2.Task5.V15.Lib
+{
+    public class DataService : ISprint2Task5V15
+    {
+        public string FindDayName(int k)
+        {
+            
+            int dayIndex = (k - 1) % 7;
+
+            
+            switch (dayIndex)
+            {
+                case 0:
+                    return "Понедельник";
+                case 1:
+                    return "Вторник";
+                case 2:
+                    return "Среда";
+                case 3:
+                    return "Четверг";
+                case 4:
+                    return "Пятница";
+                case 5:
+                    return "Суббота";
+                case 6:
+                    return "Воскресенье";
+                default:
+                    
+                    return "Неверный день";
+            }
+        }
+
+    }
+}
---
+++ Tyuiu.FilimonchukED.Sprint2.Task5.V15/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.FilimonchukED.Sprint2.Task5.V15.Lib;
+namespace Tyuiu.FilimonchukED.Sprint2.Task5.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Филимончук Е. Д. | РППб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнил: Филимончук Егор Дмитриевич | РППб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch вычисляет        *");
+            Console.WriteLine("* требуемое значение и возвращает результат.                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+            Console.WriteLine("Введите день:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"{x} это " + ds.FindDayName(x));
+
+            Console.ReadKey();
+        }
+    }
+}
