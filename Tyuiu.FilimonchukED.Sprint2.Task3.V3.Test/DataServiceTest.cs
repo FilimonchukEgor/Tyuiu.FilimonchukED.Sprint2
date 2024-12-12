73ccc0484832af48a3e079b768c92f60b38103d7
Ернар Пупкин <gamerhey2@gmail.com>
2024-12-12 14:08:56 +0500

    Task3

---
+++ Tyuiu.FilimonchukED.Sprint2.Task3.V3.Test/DataServiceTest.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.FilimonchukED.Sprint2.Task3.V3.Lib;
+namespace Tyuiu.FilimonchukED.Sprint2.Task3.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = ds.Calculate(x);
+            double wait = 18;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 0.167;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            DataService ds = new DataService();
+            double x = -2;
+            double res = ds.Calculate(x);
+            double wait = 0.64;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            DataService ds = new DataService();
+            double x = -14;
+            double res = ds.Calculate(x);
+            double wait = 56.071;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.FilimonchukED.Sprint2.Task3.V3/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.FilimonchukED.Sprint2.Task3.V3.Lib;
+namespace Tyuiu.FilimonchukED.Sprint2.Task3.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Филимончук Е. Д. | РППб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Вложенные операторы if - else                                     *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Филимончук Егор Дмитриевич | РППб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x;
+            Console.WriteLine("Введите значение Х=");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Учитывая условия, ответ будет = " + ds.Calculate(x));
+
+        }
+    }
+}
