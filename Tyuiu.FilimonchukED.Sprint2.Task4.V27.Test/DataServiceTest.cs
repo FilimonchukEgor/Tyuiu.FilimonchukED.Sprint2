68834b2e283ee45c61de8b91562aa9b852ca6527
Ернар Пупкин <gamerhey2@gmail.com>
2024-12-12 14:57:21 +0500

    Task4

---
+++ Tyuiu.FilimonchukED.Sprint2.Task4.V27.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.FilimonchukED.Sprint2.Task4.V27.Lib;
+namespace Tyuiu.FilimonchukED.Sprint2.Task4.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 20;
+            double y = 5;
+            double res = ds.Calculate(x, y);
+            double wait = 0;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            DataService ds = new DataService();
+            double x = 15;
+            double y = 10;
+            double wait = 99.867;
+            double res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.FilimonchukED.Sprint2.Task4.V27/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.FilimonchukED.Sprint2.Task4.V27.Lib;
+namespace Tyuiu.FilimonchukED.Sprint2.Task4.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Филимончук Е. Д. | РППб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнил: Филимончук Егор Дмитриевич | РППб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу по формуле                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x, y;
+            Console.WriteLine("Введите значения для Х=");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение для Y=");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Ответ = " + ds.Calculate(x, y));
+        }
+    }
+}
